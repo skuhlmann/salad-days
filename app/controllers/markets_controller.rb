efd6572c5cbecf59864af0a711d55fcb669e5248
Sam Kuhlmann <samkuhlmann@gmail.com>
2015-01-18 15:15:35 -0700

    Adds notice partial and places notice message

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -1,6 +1,6 @@
 class MarketsController < ApplicationController
-  before_action :set_market, only: [:show, :edit, :update, :destory]
-  before_action :require_market_owner, only: [:edit, :update, :destroy]
+  before_action :set_market, only: [:show, :edit, :destory]
+  before_action :require_market_owner, only: [:edit, :destroy]
 
   def show
   end
@@ -23,9 +23,11 @@ class MarketsController < ApplicationController
   end
 
   def update
+    @market = Market.find(params[:slug])
+    require_market_owner
     if @market.update_attributes(market_params)
-      flash[:notice] = "Market was successfully updated."
-      redirect_to market_path(@market.slug)
+      flash[:notice] = "Market details updated."
+      redirect_to user_path(current_user)
     else
       render :edit
     end
--- spec/features/market_owner_user_spec.rb
+++ spec/features/market_owner_user_spec.rb
@@ -24,11 +24,38 @@ describe "Market owner", type: :feature do
   it "can see thier existing market options from thier profile page" do
     user = User.last
     market = create(:market, user: user)
+
     visit user_path(user)
+
     expect(page).to have_text("The Market")
     expect(page).to have_text("email@example.com")
   end
 
+  it "can see view listing for thier market" do
+    user = User.last
+    market = create(:market, user: user)
+    listing = create(:listing, market: market)
+
+    visit user_path(user)
+
+    expect(page).to have_text("Current Listings")
+    expect(page).to have_text("A Listing")
+    expect(page).to have_text("Listing details")
+    expect(page).to have_text(listing.harvest_date)
+  end
+
+  it "can can edit thier market" do
+    user = User.last
+    market = create(:market, user: user)
+
+    visit user_path(user)
+    click_link_or_button("Edit your market")
+    fill_in("market_name", with: "Race Street Farms")
+    click_button("Update Market")
+
+    expect(page).to have_text("Market details updated")
+    expect(page).to have_text("Race Street Farms")
+  end
 end
 
 
