704cb0f8ab79d1738d9f0755cdc1d82c1cf4ccd8
Sam Kuhlmann <samkuhlmann@gmail.com>
2015-01-25 10:37:18 -0700

    Simplifies search result class

--- app/models/search_result.rb
+++ app/models/search_result.rb
@@ -1,35 +1,18 @@
 class SearchResult
 
   def self.find_results(zip)
-    build_combined_collection(
-      build_farmers_collection(fetch_farmers_markets(zip)),
-      build_users_collection(fetch_users_markets(zip))
-    ).sort {|a, b| a.first <=> b.first }
+    sort_by_distance(farmers_markets(zip).concat(users_markets(zip)))
   end
 
-  private
-
-  def self.fetch_farmers_markets(zip)
+  def self.farmers_markets(zip)
     FarmersMarket.find_all_by(zip)
   end
 
-  def self.fetch_users_markets(zip)
+  def self.users_markets(zip)
     Market.near(zip, 25)
   end
 
-  def self.build_farmers_collection(markets)
-    markets.map { |market| [distance_from(market.marketname), market] }
-  end
-
-  def self.build_users_collection(markets)
-    markets.map { |market| [market.distance.round(1), market] }
-  end
-
-  def self.distance_from(marketname)
-    marketname.split(" ")[0].to_f
-  end
-
-  def self.build_combined_collection(farmers_market, users_market)
-    farmers_market.concat(users_market)
+  def self.sort_by_distance(markets)
+    markets.sort_by { |market| market.distance }
   end
 end
